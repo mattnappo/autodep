003d1ca1ae92a515856046269b7fa8f6d878eae4
Matt Nappo <mattnappo@gmail.com>
2023-12-05 16:14:36 -0500

    Much better tests

--- tests/bench.sh
+++ tests/bench.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ali --duration=10s \
-    --rate=20 \
-    --workers=5 \
+ali --duration=5s \
+    --rate=35 \
+    --workers=12 \
     --header 'Content-Type: application/json' \
     --body-file=$1 \
     --method=POST http://localhost:9000/inference
--- tests/status.py
+++ tests/status.py
@@ -1,9 +1,16 @@
 import requests, json, time
 from collections import Counter
 
+curr = -1
 while True:
-    res = requests.get("http://localhost:9000/workers")
+    res = requests.get("http://localhost:9000/workers/_status")
     status = res.json()
-    print(status)
-    print()
+    count = Counter(status.values())
+    working = count['Working']
+    if working != curr:
+        curr = working
+        print(f"Active workers: {curr}")
+
     time.sleep(0.05)
+
+
